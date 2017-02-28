700fc396ee8deadd53a298587534e9c231278d63
dani <dani.uni1137@gmail.com>
2017-02-28 12:02:35 +0100

    Problema2 - Primera proba

--- Problema2/main.py
+++ Problema2/main.py
@@ -3,15 +3,34 @@
 # vim: set fileencoding=utf8 :
 
 import sys
+import urllib2
 
 api_key = None
 
 
 class WeatherClient(object):
     """docstring foe WeatherClient"""
+    url_base = "http://api.wunderground.com/api/"
+    url_service = {
+        "almanac": "/almanac/q/CA/"
+    }
     def __init__(self, api_key):
         super(WeatherClient, self).__init__()
         self.api_key = api_key
+
+    def almanac(self, location):
+        # baixar-se la web
+        url = self.url_base + self.api_key + self.url_service["almanac"] + \
+            location + ".xml"
+        print url
+        f = urllib2.urlopen(url)
+        data = f.read()
+        f.close()
+
+        # llegir-la
+
+        # retornar resultats
+        return data
 
 
 if __name__ == "__main__":
@@ -23,3 +42,4 @@ if __name__ == "__main__":
 
     wc = WeatherClient(api_key)
     result = wc.almanac("Lleida")
+    print result
